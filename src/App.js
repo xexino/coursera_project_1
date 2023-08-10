707b691937bda4f59ecf58add0eca03fe620d3e8
xexino <89513742+xexino@users.noreply.github.com>
2023-08-10 15:06:28 +0200

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { Navbar, NavbarBrand } from "reactstrap";
+import Menu from "./components/MenuComponent";
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+        <Menu />
       </div>
     );
   }
